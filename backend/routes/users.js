8f5c65e91f6130b8affaa24c6485a335e0d7f4ed
Samm96 <sihorsch96@gmail.com>
2022-07-10 18:50:57 -0400

    Add: create login & registration routes / moved post create user to app.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,11 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const { linkRegex, emailRegex } = require('../utils/regex');
 
 const {
-  getUser, getUsers, createUser, updateUser, updateAvatar,
+  getUser, getUsers, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
-router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(linkRegex),
-    email: Joi.string().email(emailRegex).required(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
