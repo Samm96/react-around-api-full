b7f912751a17444daa29d831dadeb485b39bcd13
Samm96 <sihorsch96@gmail.com>
2022-07-10 19:17:32 -0400

    Fix: removed userJoi, reverted back to appjs having celebrate

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-const { userLogin, createUser } = require('./controllers/users');
-const { celebrate } = require('celebrate');
-const userJoi = require('./utils/utils');
+const { userLogin, createUser } = require("./controllers/users");
+const { celebrate, Joi } = require("celebrate");
+const { linkRegex, emailRegex } = require("./utils/regex");
 
 const { PORT = 3000 } = process.env;
 
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/aroundb');
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,14 +21,26 @@ app.use(express.urlencoded({ extended: true }));
 // temp authorization solution
 app.use((req, res, next) => {
   req.user = {
-    _id: '62a7e195f4764aa1f59daf17',
+    _id: "62a7e195f4764aa1f59daf17",
   };
 
   next();
 });
 
-app.post('/signin', userLogin);
-app.post('/signup', celebrate(userJoi), createUser);
+app.post("/signin", userLogin);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(linkRegex),
+      email: Joi.string().email(emailRegex).required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser
+);
 
 app.use(routes);
 
