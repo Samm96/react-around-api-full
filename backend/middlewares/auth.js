11adef495ada5c64d06990d3d2c62bb9bf7842f3
Samm96 <sihorsch96@gmail.com>
2022-07-10 19:39:25 -0400

    Add: middleware mkdir and authjs / add: middleware code

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,26 @@
+const {
+    AUTHORIZATION_ERROR_CODE,
+  } = require("../utils/errors");
+const jwt = require("jsonwebtoken");
+const { secretKey } = require("../utils/utils");
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(AUTHORIZATION_ERROR_CODE).send({ message: 'Authorization required'});
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+
+    try {
+        payload = jwt.verify(token, secretKey);
+    } catch (err) {
+        res.status(AUTHORIZATION_ERROR_CODE).send({ message: 'Authorization required'});
+    }
+
+    req.user = payload; // assigns payload to request object
+
+    next();
+}
