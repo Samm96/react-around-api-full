d8658b26ef8cf01dd43a3cddb274335dfa968266
Samm96 <sihorsch96@gmail.com>
2022-07-16 22:42:14 -0400

    Add: celebrate joi to create card in routes/cardsjs

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
-const { requestLogger, errorLogger } = require('../middleware/logger');
+const { celebrate, Joi } = require('celebrate');
+const { requestLogger } = require('../middleware/logger');
+const { linkRegex } = require('../utils/regex');
+const validateURL = require('../utils/urlValidate');
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', requestLogger, createCard);
+router.post('/', requestLogger, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().regex(linkRegex).custom(validateURL),
+  }),
+}), createCard);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
