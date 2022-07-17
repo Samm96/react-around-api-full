b47b6c789ebdcb0bbbe60537e91814bff29108ee
Samm96 <sihorsch96@gmail.com>
2022-07-17 16:13:11 -0400

    Add: CORS

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes');
 const { errorLogger, requestLogger } = require('./middleware/logger');
+const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,6 +17,27 @@ mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    allowedCors,
+  );
+  res.header(
+    'Access-Content-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    DEFAULT_ALLOWED_METHODS,
+  );
+  next();
+});
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(requestLogger);
 
--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -5,4 +5,13 @@ const secretKey = crypto.randomBytes(32).toString('hex');
 
 const SUCCESS_MSG = 201;
 
-module.exports = { secretKey, SUCCESS_MSG };
+// CORS
+const allowedCors = 'https://api.samantha-horsch-around-us.students.nomoredomainssbs.ru';
+const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+
+module.exports = {
+  secretKey,
+  SUCCESS_MSG,
+  allowedCors,
+  DEFAULT_ALLOWED_METHODS,
+};
