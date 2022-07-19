c928c9d9dfd5dd608d1043bdf77c384e7e12f949
Samm96 <sihorsch96@gmail.com>
2022-07-18 23:00:23 -0400

    Add: more allowed cors

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,7 +1,12 @@
 const SUCCESS_MSG = 201;
 
 // CORS
-const allowedCors = 'https://api.samantha-horsch-around-us.students.nomoredomainssbs.ru';
+const allowedCors = [
+  'https://api.samantha-horsch-around-us.students.nomoredomainssbs.ru',
+  'https://www.samantha-horsch-around-us.students.nomoredomainssbs.ru',
+  'https://samantha-horsch-around-us.students.nomoredomainssbs.ru',
+  'localhost:3000',
+];
 const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
 
 module.exports = {
