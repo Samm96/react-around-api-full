dd3e624c2c58c25929d160b55078a9f92fe3b656
Samm96 <sihorsch96@gmail.com>
2022-07-19 16:26:22 -0400

    Adjust backend/appjs

--- backend/app.js
+++ backend/app.js
@@ -20,18 +20,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    allowedCors,
-  );
-  res.header(
-    'Access-Control-Request-Methods',
-    'GET, HEAD, PUT, PATCH, POST, DELETE',
-  );
-  res.header(
-    'Access-Content-Control-Request-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'api.samantha-horsch-around-use.students.nomoredomainssbs.ru',
+    );
+    res.header(
+      'Access-Control-Request-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.header(
+      'Access-Content-Control-Request-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+  }
+
   next();
 });
 
