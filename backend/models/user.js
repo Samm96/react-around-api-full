cc831bbe6d69e9874b36a0ffeca605f687fc021b
Samm96 <sihorsch96@gmail.com>
2022-07-22 17:09:23 -0400

    Edit: ran prettier

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,49 +2,52 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const { linkRegex, emailRegex } = require('../utils/regex');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    default: 'Jacques Cousteau',
-    minLength: [2, 'The NAME field length is too short (min: 2)'],
-    maxLength: [30, 'The NAME field length is too long (max: 30)'],
-    required: [true, 'Required field'],
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: 'Jacques Cousteau',
+      minLength: [2, 'The NAME field length is too short (min: 2)'],
+      maxLength: [30, 'The NAME field length is too long (max: 30)'],
+      required: [true, 'Required field'],
+    },
 
-  about: {
-    type: String,
-    default: 'Explorer',
-    minLength: [2, 'The ABOUT field length is too short (min: 2)'],
-    maxLength: [30, 'The ABOUT field length is too long (max: 30)'],
-    required: [true, 'Required field'],
-  },
+    about: {
+      type: String,
+      default: 'Explorer',
+      minLength: [2, 'The ABOUT field length is too short (min: 2)'],
+      maxLength: [30, 'The ABOUT field length is too long (max: 30)'],
+      required: [true, 'Required field'],
+    },
 
-  avatar: {
-    type: String,
-    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
-    required: [true, 'Required field'],
-    validate: {
-      validator: (v) => linkRegex.test(v),
-      message: 'This is not a valid URL',
+    avatar: {
+      type: String,
+      default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
+      required: [true, 'Required field'],
+      validate: {
+        validator: (v) => linkRegex.test(v),
+        message: 'This is not a valid URL',
+      },
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Required field'],
+      unique: true,
+      validate: {
+        validator: (v) => emailRegex.test(v),
+        message: 'This is not a valid email',
+      },
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Required field'],
+      select: false,
     },
   },
-
-  email: {
-    type: String,
-    required: [true, 'Required field'],
-    unique: true,
-    validate: {
-      validator: (v) => emailRegex.test(v),
-      message: 'This is not a valid email',
-    },
-  },
-
-  password: {
-    type: String,
-    required: [true, 'Required field'],
-    select: false,
-  },
-}, { versionKey: false });
+  { versionKey: false },
+);
 
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
@@ -54,14 +57,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
         return Promise.reject(new Error('Incorrect email or password'));
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error('Incorrect email or password'));
-          }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Incorrect email or password'));
+        }
 
-          return user;
-        });
+        return user;
+      });
     });
 };
 
