c182e16a05d8cdfed85613cdb89b84c7a2b72880
Samm96 <sihorsch96@gmail.com>
2022-08-05 14:01:40 -0400

    Fix: added more joi validation to backend updateuser

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,6 +18,8 @@ router.get('/me', getCurrentUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     ObjectId: Joi.string().hex().length(24),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), updateUser);
 
